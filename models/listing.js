298ed0810a8fcd352af90e7e1da54b3db4781e7d
DhirajKumar <kumardhiraj97125@gmail.com>
2026-05-25 17:37:12 +0530

    Initial project setup

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default:
+            "https://unsplash.com/photos/sunflower-field-during-day-time-lk3F07BN8T8",
+        set: (v) => v === "" ? "https://unsplash.com/photos/sunflower-field-during-day-time-lk3F07BN8T8" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
